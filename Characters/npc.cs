4a57c18f43cbca0c55daabea78d3d463452ff86e
nonagaga <gabrielreyeswagner@gmail.com>
2024-06-13 21:30:59 +0900

    NPCS now random

--- Characters/npc.cs
+++ Characters/npc.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 
 public partial class npc : CharacterBody2D
 {
+    private static RandomNumberGenerator rng = new RandomNumberGenerator();
+
     private NavigationAgent2D _navigationAgent;
     private Timer _timer;
 
@@ -37,6 +39,9 @@ public partial class npc : CharacterBody2D
                 _movementTargets.Add(marker2D.GlobalPosition);
             }
         }
+
+        //pick a random target to go to
+        _currentTargetIdx = rng.RandiRange(0, _movementTargets.Count-1);
             
 
         // These values need to be adjusted for the actor's speed
@@ -47,8 +52,12 @@ public partial class npc : CharacterBody2D
         // Make sure to not await during _Ready.
         Callable.From(ActorSetup).CallDeferred();
 
-        //register the nav agent to the navserver for avoidance
-        NavigationServer2D.AgentSetAvoidanceCallback(_navigationAgent.GetRid(), new Callable(this, MethodName.AvoidanceCompleted));
+        if ((bool) _navigationAgent.Get("avoidance_enabled"))
+        {
+            //register the nav agent to the navserver for avoidance
+            NavigationServer2D.AgentSetAvoidanceCallback(_navigationAgent.GetRid(), new Callable(this, MethodName.AvoidanceCompleted));
+        }
+        
     }
 
     public override void _PhysicsProcess(double delta)
@@ -59,7 +68,6 @@ public partial class npc : CharacterBody2D
         {
             if (_timer.TimeLeft == 0)
             {
-                RandomNumberGenerator rng = new RandomNumberGenerator();
                 _timer.WaitTime = rng.RandiRange(3, 8);
                 _timer.Start();
             }
@@ -71,7 +79,20 @@ public partial class npc : CharacterBody2D
         Vector2 currentAgentPosition = GlobalTransform.Origin;
         Vector2 nextPathPosition = _navigationAgent.GetNextPathPosition();
 
-        _navigationAgent.Velocity = currentAgentPosition.DirectionTo(nextPathPosition) * _movementSpeed;
+        //calculate our velocity in a temp variable
+        Vector2 vel = currentAgentPosition.DirectionTo(nextPathPosition) * _movementSpeed;
+
+        //if we have disabled avoidance, change velocity and do the move and slide immediately
+        if (!(bool)_navigationAgent.Get("avoidance_enabled"))
+        {
+            Velocity = vel;
+            MoveAndSlide();
+        }
+        else
+        {
+            //add our velocity to our nav agent for avoidance calculation
+            _navigationAgent.Velocity = vel;
+        }
     }
 
     private async void ActorSetup()
@@ -82,13 +103,19 @@ public partial class npc : CharacterBody2D
         // Now that the navigation map is no longer empty, set the movement target.
         if (_movementTargets.Count != 0)
         {
-            MovementTarget = _movementTargets[_currentTargetIdx];
+            SetMovementTargetWithRadius(_movementTargets[_currentTargetIdx], 5f);
         }
+    }
+
+    private void SetMovementTargetWithRadius(Vector2 target, float radius)
+    {
+        MovementTarget = target + new Vector2(rng.RandfRange(-1, 1),rng.RandfRange(-1, 1)).Normalized() * radius;
     }
 
     //does the actual obstacle avoidance
     private void AvoidanceCompleted(Vector3 newVelocity)
     {
+        //now that the avoidance velocity has been calculated, apply the corrected velocity and move and slide
         Velocity = new Vector2(newVelocity.X, newVelocity.Z);
         MoveAndSlide();
     }
@@ -96,14 +123,24 @@ public partial class npc : CharacterBody2D
     public void OnTimerTimeout()
     {
         {
-            _currentTargetIdx += 1;
+
+            //a simple but effective way to prevevnt picking the same table twice
+
+            var newTargetIdx = _currentTargetIdx;
+
+            while (newTargetIdx == _currentTargetIdx)
+            {
+                newTargetIdx = rng.RandiRange(0, _movementTargets.Count - 1);
+            }
+
+            _currentTargetIdx = newTargetIdx;
 
             if (_currentTargetIdx >= _movementTargets.Count)
             {
                 _currentTargetIdx = 0;
             }
 
-            MovementTarget = _movementTargets[_currentTargetIdx];
+            SetMovementTargetWithRadius(_movementTargets[_currentTargetIdx], 5f);
         };
     }
 }
