ef4d75d2a8e7e39afc9c476e8dff6513a009ff29
hailey-pham <haileypham@gmail.com>
2024-06-12 23:54:03 -0400

    Added Store Stats

---
+++ Scripts/StoreStats.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public partial class StoreStats : Node
+{
+	int money = 0;
+	String[] items = {};
+	int[] itemStock = {};
+	int stock;
+	bool[] indoorUpgrades = {};
+	bool[] outdoorUpgrades = {};
+}
