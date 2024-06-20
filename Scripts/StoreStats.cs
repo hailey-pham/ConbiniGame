723235605f8db5170e0463c3d85d85f34f208117
hailey-pham <haileypham@gmail.com>
2024-06-20 03:08:52 -0400

    Restock !!!

--- Scripts/StoreStats.cs
+++ Scripts/StoreStats.cs
@@ -4,8 +4,8 @@ using System;
 public partial class StoreStats : Node
 {
 	int money = 0;
-	String[] items = {};
-	int[] itemStock = {};
+	String[] items = {"Water", "Food"};
+	int[] itemStock = {0, 0};
 	int stock;
 	bool[] indoorUpgrades = {};
 	bool[] outdoorUpgrades = {};
