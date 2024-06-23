aa8b23621afc0a4a07456691c05e8f827bf4badf
yavna <chandyavna@ufl.edu>
2024-06-23 21:31:03 +0900

    horrible bad code idk what i did

--- Scripts/Calendar.cs
+++ Scripts/Calendar.cs
@@ -12,7 +12,7 @@ public partial class Calendar : Node2D
     private int dayPercent = 0;
 
     private int elapsedTime = 0;
-    private const int dayLength = 2; // 2 seconds long for testing purposes, change to 10 * 60 for the actual game
+    private const int dayLength = 1 * 60; // 2 seconds long for testing purposes, change to 10 * 60 for the actual game
     private const int seasonLength = 7; // 7 days per season, or maybe 5?
 
     private int currentDay = 1;
@@ -23,7 +23,8 @@ public partial class Calendar : Node2D
     private Label calendarLabel;
     private string[] seasons = { "Spring", "Summer", "Autumn", "Winter" };
 
-    public SceneManager sceneManager { get; set; }
+    private PackedScene endOfDayMenuScene;
+
 
     public override void _Ready()
     {
@@ -35,8 +36,6 @@ public partial class Calendar : Node2D
         // get the label nodes
         timeLabel = GetNode<Label>("TimeLabel");
         calendarLabel = GetNode<Label>("CalendarLabel");
-
-        sceneManager = GetTree().Root.GetNode<SceneManager>("SceneManager");
 
         CustomizeLabels();
 
@@ -72,7 +71,6 @@ public partial class Calendar : Node2D
             else
             {
                 UpdateCalendarLabel();
-                ShowEndOfDayMenu();
             }
         }
         else
@@ -99,19 +97,8 @@ public partial class Calendar : Node2D
         GD.Print("Season has changed to: " + currentSeasonStr);
         UpdateCalendarLabel();
     }
-    private void ShowEndOfDayMenu()
-    {
-        if (sceneManager != null)
-        {
-            sceneManager.ShowEndOfDayMenu();
-            GetTree().Paused = true;
-        }
-        else
-        {
-            GD.PrintErr("SceneManager is not found!");
-        }
-    }
 
+ 
     private void CustomizeLabels()
     {
         // in case we want to customize labels further
