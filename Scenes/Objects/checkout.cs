c1dcb91706207a32c9afa0ffb654b6510733f3cd
nonagaga <gabrielreyeswagner@gmail.com>
2024-07-08 14:35:52 +0900

    Added checkout beeping noises

--- Scenes/Objects/checkout.cs
+++ Scenes/Objects/checkout.cs
@@ -3,6 +3,7 @@ using Godot.Collections;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public partial class checkout : StaticBody2D
 {
@@ -10,30 +11,32 @@ public partial class checkout : StaticBody2D
 	[Export]
 	private PackedScene sellAnimation;
 
-	Timer Timer;
-	Area2D PlayerCheck;
+	private Timer timer;
+	private Area2D playerCheck;
+	private AudioStreamPlayer audioStreamPlayer;
 
 	Queue<npc> npcs = new Queue<npc>();
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		PlayerCheck = GetNode<Area2D>("PlayerCheck");
-		Timer = GetNode<Timer>("Timer");
+		playerCheck = GetNode<Area2D>("PlayerCheck");
+		timer = GetNode<Timer>("Timer");
+		audioStreamPlayer = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
 
-		Timer.Timeout += OnTimerTimeout;
+		timer.Timeout += OnTimerTimeout;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		//if player is standing at the counter...
-		if (PlayerCheck.HasOverlappingBodies())
+		//if player is standing at the counter and NPC is waiting...
+		if (playerCheck.HasOverlappingBodies() && npcs.Count > 0)
 		{
-			Timer.Paused = false;
+			timer.Paused = false;
 		} else
 		{
-			Timer.Paused = true;
+			timer.Paused = true;
 		}
 	}
 
@@ -47,25 +50,41 @@ public partial class checkout : StaticBody2D
 	public void OnTimerTimeout()
 	{
 		if (npcs.Count > 0)
-		{
-            var npc = npcs.Dequeue();
-			npc.ShoppingCart.ForEach(SellItem);
-            npc.stateMachine.TransitionTo("LeaveState");
+		{	
+			var npc = npcs.First();
+			if (npc.ShoppingCart.Count > 0)
+			{
+                var item = npc.ShoppingCart[0];
+				npc.ShoppingCart.RemoveAt(0);
+
+				SellItem(item);
+				if(npc.ShoppingCart.Count == 0)
+				{
+					//we have sold everything in the npc's cart
+					npcs.Dequeue();
+                    npc.stateMachine.TransitionTo("LeaveState");
+                }
+            }
         }
 
-        Timer.Start();
+        timer.Start();
     }
 
 	public void SellItem(ItemRes item)
 	{
+		//animate the items profit
         var anim = sellAnimation.Instantiate();
         var animScript = anim as sell_animation;
 		animScript.Value = item.price;
+        AddChild(anim);
 
+		//play the sound
+		audioStreamPlayer.Play();
+
+		//update global variables
         var globals = GetNode<globals>("/root/Globals");
 		globals.Earnings += item.price;
         globals.Money += item.price;
 		globals.Customers += 1;
-		AddChild(anim);
     }
 }
