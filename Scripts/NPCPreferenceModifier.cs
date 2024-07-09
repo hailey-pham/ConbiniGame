880c10d47917876d6a53d84dd871b6e58f4c257d
nonagaga <gabrielreyeswagner@gmail.com>
2024-07-09 09:57:09 +0900

    Basic skeleton for NPC item preferences :+1:

--- Scripts/NPCPreferenceModifier.cs
+++ Scripts/NPCPreferenceModifier.cs
@@ -6,19 +6,39 @@ public partial class NPCPreferenceModifier : Node
     private Calendar calendar;
     public float ItemBuyProbability(ItemRes item)
     {
-        //have yet to add disaster logic
+        //we assume that npc's don't want to buy item
+        float prob = 0f;
 
+        //a short way of saying if calendar is null, get the calendar node
         calendar ??= GetNode<Calendar>("/root/Calendar");
 
+        //replace this with a call to calendar to get the next disaster
+        //0 - no disaster
+        //Earthquake = 1
+        //Tsunami = 2
+        //Typhoon = 3
+        //WildFire = 4
+        //FlashFlood = 5
+        //HeavySnow = 6
+
+        //this is temporary
+        var tomorrowDisasterInt = 1;
+
+        //increase buy probability if disaster lines up
+        if (item.DisasterFlags.HasFlag((ItemRes.DisastersEnum)(1 << tomorrowDisasterInt)))
+        {
+            prob += 0.5f;
+        }
+
+        //the integer of the current season in the game
         var currSeasonInt = calendar.GetCurrentSeason();
 
+        //increase buy probability if season lines up
         if (item.SeasonFlags.HasFlag((ItemRes.SeasonsEnum)(1 << currSeasonInt)))
         {
-            return 1f;
+            prob += 0.5f;
         }
-        else
-        {
-            return 0.0f;
-        }
+
+        return prob;
     }
 }
