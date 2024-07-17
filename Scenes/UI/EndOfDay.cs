f1eb2f8a714b690b2045f8bfa6afcd8773ed6a52
romeo <69266292+plasmamintz@users.noreply.github.com>
2024-07-17 16:53:10 +0900

    i think i can just commit this?? splash

--- Scenes/UI/EndOfDay.cs
+++ Scenes/UI/EndOfDay.cs
@@ -4,104 +4,104 @@ using System;
 
 public partial class EndOfDay : Control
 {
-    [Signal]
-    public delegate void UpgradeButtonPressedEventHandler();
+	[Signal]
+	public delegate void UpgradeButtonPressedEventHandler();
 
-    [Signal]
-    public delegate void RestockButtonPressedEventHandler();
+	[Signal]
+	public delegate void RestockButtonPressedEventHandler();
 
-    [Signal]
-    public delegate void SleepButtonPressedEventHandler();
-
-
-    private Button upgradeButton;
-
-    private Button restockButton;
-    private Button sleepButton;
-
-    private RichTextLabel customersLabel;
-    private RichTextLabel earningsLabel;
-    private RichTextLabel dayLabel;
-    private RichTextLabel totalMoneyLabel;
-
-    private Label itemsSoldLabel;
-    private ItemList itemsSoldList;
-
-    private globals globals;
-    private Calendar calendar;
+	[Signal]
+	public delegate void SleepButtonPressedEventHandler();
 
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        upgradeButton = GetNode<Button>("VBoxContainer/HBoxContainer/UpgradeButton");
+	private Button upgradeButton;
 
-        restockButton = GetNode<Button>("VBoxContainer/HBoxContainer/RestockButton");
+	private Button restockButton;
+	private Button sleepButton;
 
-        sleepButton = GetNode<Button>("VBoxContainer/HBoxContainer/SleepButton");
+	private RichTextLabel customersLabel;
+	private RichTextLabel earningsLabel;
+	private RichTextLabel dayLabel;
+	private RichTextLabel totalMoneyLabel;
 
-        upgradeButton.Pressed += OnUpgradeButtonPressed;
-        restockButton.Pressed += OnRestockButtonPressed;
-        sleepButton.Pressed += OnSleepButtonPressed;
+	private Label itemsSoldLabel;
+	private ItemList itemsSoldList;
+
+	private globals globals;
+	private Calendar calendar;
 
 
-        customersLabel = GetNode<RichTextLabel>("VBoxContainer/Customers");
-        earningsLabel = GetNode<RichTextLabel>("VBoxContainer/Cash");
-        dayLabel = GetNode<RichTextLabel>("VBoxContainer/Day");
-        totalMoneyLabel = GetNode<RichTextLabel>("VBoxContainer/TotalMoney");
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		upgradeButton = GetNode<Button>("VBoxContainer/HBoxContainer/UpgradeButton");
 
-        itemsSoldLabel = GetNode<Label>("VBoxContainer2/ItemsSoldLabel");
-        itemsSoldList = GetNode<ItemList>("VBoxContainer2/ItemsSoldList");
+		restockButton = GetNode<Button>("VBoxContainer/HBoxContainer/RestockButton");
 
-        globals = GetNode<globals>("/root/Globals");
-        calendar = GetNode<Calendar>("/root/Calendar");
+		sleepButton = GetNode<Button>("VBoxContainer/HBoxContainer/SleepButton");
 
-        DisplayEndOfDayStats();
-    }
+		upgradeButton.Pressed += OnUpgradeButtonPressed;
+		restockButton.Pressed += OnRestockButtonPressed;
+		sleepButton.Pressed += OnSleepButtonPressed;
 
-    private void OnUpgradeButtonPressed()
-    {
-        GD.Print("Upgrade button pressed!");
-        // todo later: logic for getting upgrade menu up
-        EmitSignal(nameof(UpgradeButtonPressed));
-        var sceneManager = GetNode<SceneManager>("/root/SceneManager");
-        sceneManager.ChangeScene("upgrade");
-    }
 
-    private void OnRestockButtonPressed()
-    {
-        GD.Print("Restock button pressed!");
-        EmitSignal(nameof(RestockButtonPressed));
+		customersLabel = GetNode<RichTextLabel>("VBoxContainer/Customers");
+		earningsLabel = GetNode<RichTextLabel>("VBoxContainer/Cash");
+		dayLabel = GetNode<RichTextLabel>("VBoxContainer/Day");
+		totalMoneyLabel = GetNode<RichTextLabel>("VBoxContainer/TotalMoney");
 
-        var sceneManager = GetNode<SceneManager>("/root/SceneManager");
-        sceneManager.ChangeScene("restock");
-    }
+		itemsSoldLabel = GetNode<Label>("VBoxContainer2/ItemsSoldLabel");
+		itemsSoldList = GetNode<ItemList>("VBoxContainer2/ItemsSoldList");
 
-    private void OnSleepButtonPressed()
-    {
-        GD.Print("Sleep button pressed!");
-        // todo later: logic for playing sleep scene
-        EmitSignal(nameof(SleepButtonPressed));
-        calendar.DetermineNextDay();
-    }
+		globals = GetNode<globals>("/root/Globals");
+		calendar = GetNode<Calendar>("/root/Calendar");
 
-    public void DisplayEndOfDayStats()
-    {
-        customersLabel.Text = "Customers Serviced: " + globals.Customers.ToString();
-        earningsLabel.Text = "Cash Earned: " + globals.Earnings.ToString() + " yen";
-        dayLabel.Text = "End of Day: " + globals.Day.ToString();
-        totalMoneyLabel.Text = "You now have: " + globals.Money.ToString() + " yen";
-        UpdateItemsSold();
-    }
+		DisplayEndOfDayStats();
+	}
 
-    private void UpdateItemsSold()
-    {
-        var itemsSoldToday = globals.ItemsSoldToday;
+	private void OnUpgradeButtonPressed()
+	{
+		GD.Print("Upgrade button pressed!");
+		// todo later: logic for getting upgrade menu up
+		EmitSignal(nameof(UpgradeButtonPressed));
+		var sceneManager = GetNode<SceneManager>("/root/SceneManager");
+		sceneManager.ChangeScene("upgrade");
+	}
 
-        foreach (var item in itemsSoldToday)
-        {
-            var itemTags = item.GetTagsAsEmoji();
-            itemsSoldList.AddItem(item.name + " " + itemTags, item.spriteTexture, false);
-        }
-    }
+	private void OnRestockButtonPressed()
+	{
+		GD.Print("Restock button pressed!");
+		EmitSignal(nameof(RestockButtonPressed));
+
+		var sceneManager = GetNode<SceneManager>("/root/SceneManager");
+		sceneManager.ChangeScene("restock");
+	}
+
+	private void OnSleepButtonPressed()
+	{
+		GD.Print("Sleep button pressed!");
+		// todo later: logic for playing sleep scene
+		EmitSignal(nameof(SleepButtonPressed));
+		calendar.DetermineNextDay();
+	}
+
+	public void DisplayEndOfDayStats()
+	{
+		customersLabel.Text = "Customers Serviced: " + globals.Customers.ToString();
+		earningsLabel.Text = "Cash Earned: " + globals.Earnings.ToString() + " yen";
+		dayLabel.Text = "End of Day: " + globals.Day.ToString();
+		totalMoneyLabel.Text = "You now have: " + globals.Money.ToString() + " yen";
+		UpdateItemsSold();
+	}
+
+	private void UpdateItemsSold()
+	{
+		var itemsSoldToday = globals.ItemsSoldToday;
+
+		foreach (var item in itemsSoldToday)
+		{
+			var itemTags = item.GetTagsAsEmoji();
+			itemsSoldList.AddItem(item.name + " " + itemTags, item.spriteTexture, false);
+		}
+	}
 }
--- Scripts/RestockManager.cs
+++ Scripts/RestockManager.cs
@@ -154,11 +154,11 @@ public partial class RestockManager : Node
 	
 	
 	private void OnBackButtonPressed()
-    {
-        EmitSignal(nameof(BackButtonPressed));
-        var sceneManager = GetNode<SceneManager>("/root/SceneManager");
+	{
+		EmitSignal(nameof(BackButtonPressed));
+		var sceneManager = GetNode<SceneManager>("/root/SceneManager");
 		sceneManager.ChangeScene("endofdayscene");
-    }
+	}
 
 	private void OnItemButtonPressed(string itemName)
 	{
