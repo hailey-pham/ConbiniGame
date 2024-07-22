3f8aa08e9f8b6e63334ce612cd5f08ae38721655
Meowzszs <tkiddy231@gmail.com>
2024-07-22 14:06:06 +0900

    added a game over scene, and less coutners :3
    AHH
    How do I do scene manager?

--- Scripts/Upgrade Scripts/PlayerStackItem.cs
+++ Scripts/Upgrade Scripts/PlayerStackItem.cs
@@ -6,6 +6,6 @@ public partial class PlayerStackItem : Upgrade
     public override void onLevelLoad(globals Global, Node root) 
     {
         //Changes the process mode from disabled to inherit (default)
-        root.GetChild<player>(2).IsStackItemUpgrade = true; //Should give Item Spawner 2.
+        root.GetChild<player>(1).IsStackItemUpgrade = true; //Should give Item Spawner 2.
     }
 }
