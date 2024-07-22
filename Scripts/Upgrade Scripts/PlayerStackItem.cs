63df218b914995c870ac4cce48d3f4a46d39db45
Meowzszs <tkiddy231@gmail.com>
2024-07-22 14:25:49 +0900

    fixed some errors :3

--- Scripts/Upgrade Scripts/PlayerStackItem.cs
+++ Scripts/Upgrade Scripts/PlayerStackItem.cs
@@ -6,6 +6,7 @@ public partial class PlayerStackItem : Upgrade
     public override void onLevelLoad(globals Global, Node root) 
     {
         //Changes the process mode from disabled to inherit (default)
-        root.GetChild<player>(1).IsStackItemUpgrade = true; //Should give Item Spawner 2.
+        player Player = (player)root.GetTree().GetFirstNodeInGroup("player");
+        Player.IsStackItemUpgrade = true; //Should give Item Spawner 2.
     }
 }
