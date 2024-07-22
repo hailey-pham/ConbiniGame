b182cd3f3c72c2e6937f92c02c492283f9b4e10e
nonagaga <gabrielreyeswagner@gmail.com>
2024-07-22 17:48:22 +0900

    tile map changed and main music loops

--- Scenes/World.cs
+++ Scenes/World.cs
@@ -6,8 +6,15 @@ public partial class World : Node2D
 {
 	// Called when the node enters the scene tree for the first time.
 	globals globals;
+	private AudioStreamPlayer audioPlayer;
 	public override void _Ready()
 	{
+		audioPlayer = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
+		audioPlayer.Finished += () =>
+		{
+			audioPlayer.Play();
+		};
+
 		globals = GetNode<globals>("/root/Globals");
 
 		foreach (KeyValuePair<string, Upgrade> upgrade in globals.Upgrades)
