ed4a682ec8e7acd3f9ae83b1c0edc3dabd4378ec
nonagaga <gabrielreyeswagner@gmail.com>
2024-07-23 10:50:14 +0900

    New battery?

--- Scenes/UI/SplashAnimation.gd
+++ Scenes/UI/SplashAnimation.gd
@@ -12,14 +12,14 @@ func _ready():
 	animPlayer.connect("animation_finished", on_anim_finished)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_pressed("Interact"):
 		animPlayer.play("slide")
 		animPlayer.advance(8)
 		on_anim_finished("")
 	pass
 
-func on_anim_finished(animName : String):
+func on_anim_finished(_animName : String):
 	sceneManager.ChangeScene("mainmenu","FadeToBlack")
 
 
